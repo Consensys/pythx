3e600dc87b122ca5c5b7606ce51c5c39a97e58d7
Dominik Muhs <dmuhs@protonmail.ch>
2019-02-08 12:13:43 +0100

    Add analysis submission request model

--- pythx/core/request/models.py
+++ pythx/core/request/models.py
@@ -1,4 +1,4 @@
-from typing import Dict, Any
+from typing import Dict, Any, List
 import json
 from datetime import datetime
 import dateutil.parser
@@ -6,6 +6,7 @@ from pythx.core.exceptions import RequestValidationError, RequestDecodeError
 
 
 ANALYSIS_LIST_KEYS = ("offset", "dateFrom", "dateTo")
+ANALYSIS_SUBMISSION_KEYS = ("bytecode", "sources")
 
 
 class AnalysisListRequest:
@@ -52,7 +53,84 @@ class AnalysisListRequest:
 
 
 class AnalysisSubmissionRequest:
-    pass
+    def __init__(
+        self,
+        contract_name=None,
+        bytecode=None,
+        source_map=None,
+        deployed_bytecode=None,
+        deployed_source_map=None,
+        sources=None,
+        source_list=None,
+        solc_version=None,
+        analysis_mode="quick",
+    ):
+        self.contract_name: str = contract_name
+        self.bytecode: str = bytecode
+        self.source_map: str = source_map
+        self.deployed_bytecode: str = deployed_bytecode
+        self.deployed_source_map: str = deployed_source_map
+        self.sources: Dict[str, Dict[str, str]] = sources
+        self.source_list: List[str] = source_list
+        self.solc_version: str = solc_version
+        self.analysis_mode: str = analysis_mode
+
+    def validate(self):
+        valid = True
+        msg = "Error validating analysis submission request: {}"
+        if self.analysis_mode not in ("full", "quick"):
+            valid = False
+            msg = msg.format("Analysis mode must be one of {full,quick}")
+        elif not (self.bytecode or self.sources):
+            valid = False
+            msg = msg.format("Must pass at least bytecode or source field")
+        # TODO: MOAR
+
+        if not valid:
+            raise RequestValidationError(msg)
+
+    @classmethod
+    def from_json(cls, json_str: str):
+        parsed = json.loads(json_str)
+        return cls.from_dict(parsed)
+
+    @classmethod
+    def from_dict(cls, d: Dict):
+        if type(d) is not dict or not any(k in d for k in ANALYSIS_SUBMISSION_KEYS):
+            raise RequestDecodeError(
+                "Not all required keys {} found in data {}".format(
+                    ANALYSIS_SUBMISSION_KEYS, d
+                )
+            )
+
+        # TODO: Should we validate here?
+        return cls(
+            contract_name=d.get("contractName"),
+            bytecode=d.get("bytecode"),
+            source_map=d.get("sourceMap"),
+            deployed_bytecode=d.get("deployedBytecode"),
+            deployed_source_map=d.get("deployedSourceMap"),
+            sources=d.get("sources"),
+            source_list=d.get("sourceList"),
+            solc_version=d.get("version"),
+            analysis_mode=d.get("analysisMode"),
+        )
+
+    def to_json(self):
+        return json.dumps(self.to_dict())
+
+    def to_dict(self):
+        return {
+            "contractName": self.contract_name,
+            "bytecode": self.bytecode,
+            "sourceMap": self.source_map,
+            "deployedBytecode": self.deployed_bytecode,
+            "deployedSourceMap": self.deployed_source_map,
+            "sources": self.sources,
+            "sourceList": self.source_list,
+            "version": self.solc_version,
+            "analysisMode": self.analysis_mode,
+        }
 
 
 class AnalysisStatusRequest:
