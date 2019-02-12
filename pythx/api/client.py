ab540ba79212fa253e1ba76c4e44b8079c5af691
Dominik Muhs <dmuhs@protonmail.ch>
2019-02-12 15:42:12 +0100

    Add base handler model

---
+++ pythx/api/client.py
@@ -0,0 +1,40 @@
+from pythx.api.handler import APIHandler
+
+
+class Client:
+    def __init__(self, eth_address: str, password: str, handler: APIHandler = None, access_token: str = None, refresh_token: str = None):
+        self.eth_address = eth_address
+        self.password = password
+        self.handler = handler
+        self.access_token = access_token
+        self.refresh_token = refresh_token
+
+    def login(self):
+        pass
+
+    def logout(self):
+        pass
+
+    def refresh(self):
+        pass
+
+    def analysis_list(self):
+        pass
+
+    def analyze(self):
+        pass
+
+    def status(self):
+        pass
+
+    def analysis_ready(self):
+        pass
+
+    def report(self):
+        pass
+
+    def openapi(self):
+        pass
+
+    def version(self):
+        pass
