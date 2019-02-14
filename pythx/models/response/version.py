cd41ca12cbcd4a9690a4f6c2d4c32ece54052551
Dominik Muhs <dmuhs@protonmail.ch>
2019-02-14 11:38:46 +0100

    Add hash to version response

--- pythx/models/response/version.py
+++ pythx/models/response/version.py
@@ -6,7 +6,7 @@ from pythx.models.response.analysis import Analysis
 from pythx.models.response.base import BaseResponse
 from pythx.models.response.issue import Issue, SourceFormat, SourceType
 
-VERSION_KEYS = ("api", "harvey", "mythril", "maru")
+VERSION_KEYS = ("api", "harvey", "mythril", "maru", "hash")
 
 
 class VersionResponse(BaseResponse):
@@ -16,11 +16,13 @@ class VersionResponse(BaseResponse):
         maru_version: str,
         mythril_version: str,
         harvey_version: str,
+        hashed_version: str,
     ):
         self.api_version = api_version
         self.maru_version = maru_version
         self.mythril_version = mythril_version
         self.harvey_version = harvey_version
+        self.hashed_version = hashed_version
 
     def validate(self):
         pass
@@ -36,6 +38,7 @@ class VersionResponse(BaseResponse):
             maru_version=d["maru"],
             mythril_version=d["mythril"],
             harvey_version=d["harvey"],
+            hashed_version=d["hash"],
         )
 
     def to_dict(self):
@@ -44,4 +47,5 @@ class VersionResponse(BaseResponse):
             "maru": self.maru_version,
             "mythril": self.mythril_version,
             "harvey": self.harvey_version,
+            "hash": self.hashed_version,
         }
