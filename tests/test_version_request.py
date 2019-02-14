0749991e8cd135f6812975bd44893f3fb6727811
Dominik Muhs <dmuhs@protonmail.ch>
2019-02-14 11:52:53 +0100

    Add version request test

---
+++ tests/test_version_request.py
@@ -0,0 +1,39 @@
+import json
+
+import pytest
+
+from pythx.models.exceptions import RequestDecodeError
+from pythx.models.request import VersionRequest
+
+GLOBAL = True
+
+VERSION_REQUEST = VersionRequest()
+
+
+def assert_version_request(req):
+    assert req.method == "GET"
+    assert req.headers == {}
+    assert req.parameters == {}
+    assert req.payload == {}
+    assert req.endpoint == "v1/version"
+
+
+def test_auth_logout_request_from_valid_json():
+    req = VersionRequest.from_json("{}")
+    assert_version_request(req)
+
+
+def test_auth_logout_request_from_valid_dict():
+    req = VersionRequest.from_dict({})
+    assert_version_request(req)
+
+
+def test_auth_logout_request_to_json():
+    assert VERSION_REQUEST.to_json() == "{}"
+
+
+def test_auth_logout_request_to_dict():
+    assert VERSION_REQUEST.to_dict() == {}
+
+def test_validate():
+    VERSION_REQUEST.validate()
