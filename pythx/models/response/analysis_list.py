afe1b52dc80278390499b6bcf4f4a1fb5866d4e6
Dominik Muhs <dmuhs@protonmail.ch>
2019-03-18 21:00:01 +0100

    Add analysis list magic methods and tests

--- pythx/models/response/analysis_list.py
+++ pythx/models/response/analysis_list.py
@@ -7,6 +7,7 @@ from pythx.models.response.base import BaseResponse
 from pythx.models.response.issue import Issue, SourceFormat, SourceType
 
 ANALYSIS_LIST_KEYS = ["analyses", "total"]
+INDEX_ERROR_MSG = "Analysis at index {} was not fetched"
 
 
 class AnalysisListResponse(BaseResponse):
@@ -37,3 +38,41 @@ class AnalysisListResponse(BaseResponse):
             "analyses": [a.to_dict() for a in self.analyses],
             "total": len(self.analyses),
         }
+
+    def __iter__(self):
+        for analysis in self.analyses:
+            yield analysis
+
+    def __getitem__(self, idx):
+        try:
+            return self.analyses[idx]
+        except IndexError:
+            raise IndexError(INDEX_ERROR_MSG.format(idx))
+
+    def __setitem__(self, idx, value):
+        try:
+            self.analyses[idx] = value
+        except IndexError:
+            raise IndexError(INDEX_ERROR_MSG.format(idx))
+
+    def __delitem__(self, idx):
+        try:
+            del self.analyses[idx]
+            self.total -= 1
+        except IndexError:
+            raise IndexError(INDEX_ERROR_MSG.format(idx))
+
+    def __len__(self):
+        return self.total
+
+    def __reversed__(self):
+        return reversed(self.analyses)
+
+    def __contains__(self, item):
+        if not type(item) in (Analysis, str):
+            raise ValueError("Expected type Analysis or str but got {}".format(type(item)))
+        uuid = item.uuid if type(item) == Analysis else item
+        return uuid in map(lambda x: x.uuid, self.analyses)
+
+    def __eq__(self, candidate):
+        return self.total == candidate.total and self.analyses == candidate.analyses
--- tests/test_analysis_list_response.py
+++ tests/test_analysis_list_response.py
@@ -1,5 +1,5 @@
 import json
-from copy import copy
+from copy import deepcopy
 
 import pytest
 
@@ -81,7 +81,100 @@ def test_valid_validate():
 
 
 def test_invalid_total_validate():
-    resp = copy(testdata.ANALYSIS_LIST_RESPONSE_OBJECT)
+    resp = deepcopy(testdata.ANALYSIS_LIST_RESPONSE_OBJECT)
     resp.total = -1
     with pytest.raises(ResponseValidationError):
         resp.validate()
+
+
+def test_iteration():
+    uuids = (testdata.UUID_1, testdata.UUID_2)
+    # XXX: Don't use zip here to make sure __iter__ returns
+    for idx, analysis in enumerate(testdata.ANALYSIS_LIST_RESPONSE_OBJECT):
+        assert uuids[idx] == analysis.uuid
+
+
+def test_valid_getitem():
+    for idx, analysis in list(enumerate(testdata.ANALYSIS_LIST_RESPONSE_OBJECT.analyses)):
+        assert testdata.ANALYSIS_LIST_RESPONSE_OBJECT[idx] == analysis
+
+
+def test_invalid_getitem_index():
+    with pytest.raises(IndexError):
+        testdata.ANALYSIS_LIST_RESPONSE_OBJECT[1337]
+
+
+def test_oob_getitem_slice():
+    testdata.ANALYSIS_LIST_RESPONSE_OBJECT[1337:9001] == []
+
+
+def test_valid_delitem():
+    analysis_list = deepcopy(testdata.ANALYSIS_LIST_RESPONSE_OBJECT)
+    del analysis_list[0]
+    assert analysis_list.analyses == testdata.ANALYSIS_LIST_RESPONSE_OBJECT.analyses[1:]
+    assert analysis_list.total == testdata.ANALYSIS_LIST_RESPONSE_OBJECT.total - 1
+    assert len(analysis_list) == testdata.ANALYSIS_LIST_RESPONSE_OBJECT.total - 1
+
+
+def test_invalid_delitem():
+    with pytest.raises(IndexError):
+        del testdata.ANALYSIS_LIST_RESPONSE_OBJECT[1337]
+
+
+def test_valid_setitem():
+    analysis_list = deepcopy(testdata.ANALYSIS_LIST_RESPONSE_OBJECT)
+    analysis_list[0] = "foo"
+    assert analysis_list.analyses[0] == "foo"
+    assert analysis_list[0] == "foo"
+    assert len(analysis_list.analyses) == len(testdata.ANALYSIS_LIST_RESPONSE_OBJECT.analyses)
+
+
+def test_invalid_setitem():
+    with pytest.raises(IndexError):
+        testdata.ANALYSIS_LIST_RESPONSE_OBJECT[1337] = "foo"
+
+
+def test_reversed():
+    analysis_list = deepcopy(testdata.ANALYSIS_LIST_RESPONSE_OBJECT)
+    assert list(reversed(analysis_list)) == list(reversed(testdata.ANALYSIS_LIST_RESPONSE_OBJECT.analyses))
+    assert analysis_list.total == testdata.ANALYSIS_LIST_RESPONSE_OBJECT.total
+
+
+def test_valid_object_contains():
+    assert testdata.ANALYSIS_OBJECT in testdata.ANALYSIS_LIST_RESPONSE_OBJECT
+
+
+def test_valid_uuid_contains():
+    assert testdata.ANALYSIS_OBJECT.uuid in testdata.ANALYSIS_LIST_RESPONSE_OBJECT
+
+
+def test_contains_error():
+    with pytest.raises(ValueError):
+        1 in testdata.ANALYSIS_LIST_RESPONSE_OBJECT
+
+
+def test_invalid_object_contains():
+    analysis = deepcopy(testdata.ANALYSIS_OBJECT)
+    analysis.uuid = "something else"
+    assert not analysis in testdata.ANALYSIS_LIST_RESPONSE_OBJECT
+
+
+def test_invalid_uuid_contains():
+    assert not "foo" in testdata.ANALYSIS_LIST_RESPONSE_OBJECT
+
+
+def test_list_not_equals():
+    analysis_list = deepcopy(testdata.ANALYSIS_LIST_RESPONSE_OBJECT)
+    analysis_list.total = 0
+    assert analysis_list != testdata.ANALYSIS_LIST_RESPONSE_OBJECT
+
+
+def test_nested_analysis_not_equals():
+    analysis_list = deepcopy(testdata.ANALYSIS_LIST_RESPONSE_OBJECT)
+    analysis_list.analyses[0].uuid = "foo"
+    assert analysis_list != testdata.ANALYSIS_LIST_RESPONSE_OBJECT
+
+
+def test_valid_equals():
+    analysis_list = deepcopy(testdata.ANALYSIS_LIST_RESPONSE_OBJECT)
+    assert analysis_list == testdata.ANALYSIS_LIST_RESPONSE_OBJECT
