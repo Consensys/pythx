d53b652b268c855d6bb3e35df3310f13ed7c3550
Dominik Muhs <dmuhs@protonmail.ch>
2019-03-22 16:28:04 +0100

    Add package data directories

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 """The setup script."""
 
 from setuptools import find_packages, setup
+import pythx
 
 with open("README.rst") as readme_file:
     readme = readme_file.read()
@@ -39,10 +40,14 @@ setup(
     keywords="pythx",
     name="pythx",
     packages=find_packages(),
+    package_data={
+        "pythx.models.request": "pythx/models/request/schema/*.json",
+        "pythx.models.response": "pythx/models/response/schema/*.json"
+    },
     setup_requires=setup_requirements,
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/dmuhs/pythx",
-    version="0.1.0",
+    version=pythx.__version__,
     zip_safe=False,
 )
