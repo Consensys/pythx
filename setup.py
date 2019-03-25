87c3460664e01174ec2e6ed3737e1cd92d39cc4c
Dominik Muhs <dmuhs@protonmail.ch>
2019-03-25 16:38:14 +0100

    Bump version: 1.0.0 → 1.1.0

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/dmuhs/pythx",
-    version="version='1.0.0'",
+    version="version='1.1.0'",
     zip_safe=False,
 )
