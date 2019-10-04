96d482897da3ff41b6cad7e30d8753136f4a4fa2
Dominik Muhs <dmuhs@protonmail.ch>
2019-10-04 17:45:50 +0200

    Bump version: 1.3.1 → 1.3.2

--- pythx/__init__.py
+++ pythx/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Dominik Muhs"""
 __email__ = "dominik.muhs@consensys.net"
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 
 from pythx.conf import config
 from pythx.api.client import Client
