70b6ac9f1105d8237374a22d0ad9a059324227a2
Dominik Muhs <dmuhs@protonmail.ch>
2019-11-19 12:46:41 +0100

    Bump version: 1.3.2 → 1.4.0

--- pythx/__init__.py
+++ pythx/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Dominik Muhs"""
 __email__ = "dominik.muhs@consensys.net"
-__version__ = "1.3.2"
+__version__ = "1.4.0"
 
 from pythx.conf import config
 from pythx.api.client import Client
--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/dmuhs/pythx",
-    version="1.3.2",
+    version="1.4.0",
     zip_safe=False,
 )
