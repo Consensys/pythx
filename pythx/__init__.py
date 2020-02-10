25ae13bc165b9dee0cc270f71c0aa5723e8a93f7
Dominik Muhs <dmuhs@protonmail.ch>
2020-02-10 16:47:02 +0100

    Bump version: 1.5.2 → 1.5.3

--- pythx/__init__.py
+++ pythx/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Dominik Muhs"""
 __email__ = "dominik.muhs@consensys.net"
-__version__ = "1.5.2"
+__version__ = "1.5.3"
 
 from mythx_models.exceptions import MythXAPIError, MythXBaseException, ValidationError
 
