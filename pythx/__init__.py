0dcb94db53ee09bb65704eef7a5729e5003b7418
Dominik Muhs <dmuhs@protonmail.ch>
2022-05-12 12:11:26 +0200

    Bump version: 1.7.2 → 1.7.3

--- pythx/__init__.py
+++ pythx/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Dominik Muhs"""
 __email__ = "dominik.muhs@consensys.net"
-__version__ = "1.7.2"
+__version__ = "1.7.3"
 
 from mythx_models.exceptions import MythXAPIError
 from pythx.api.client import Client
--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/dmuhs/pythx",
-    version="1.7.2",
+    version="1.7.3",
     zip_safe=False,
 )
